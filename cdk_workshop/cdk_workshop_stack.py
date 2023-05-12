ad4f7b30cbeca53fca98fe3fd159934641761567
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2023-05-12 15:59:34 -0500

    Update cdk_workshop_stack.py

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -7,6 +7,9 @@ from aws_cdk import (
     aws_iam as iam,
     aws_events as events,
     aws_events_targets as targets,
+    aws_sns as sns,
+    aws_sns_subscriptions as subscriptions,
+    aws_secretsmanager as sm
 )
 
 
@@ -16,32 +19,91 @@ class CdkWorkshopStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         
-        # Create IAM Role for Lambda to Assume
-        role = iam.Role(self, "Role",
+        # Create IAM Role for Lambdas to Assume
+        role1 = iam.Role(self, "Role1",
         assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
         role_name = "Lambda-EC2-Shutdown-Role"
     )
-        # Attaching Permissions to Role
-        role.attach_inline_policy(iam.Policy(self, "lambda-ec2-shutdown-policy",
+
+        role2 = iam.Role(self, "Role2",
+        assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+        role_name = "Lambda-RDS-Shutdown-Role"
+    )
+        role3 = iam.Role(self, "Role3",
+        assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+        role_name = "Lambda-Billing-Role"
+    )
+        # Attaching Permissions to Roles
+        role1.attach_inline_policy(iam.Policy(self, "lambda-ec2-shutdown-policy",
         statements=[iam.PolicyStatement(
             actions=["ec2:StopInstances","ec2:DescribeInstances","ec2:DescribeInstanceStatus","ec2:DescribeRegions"],
             resources=['*']
             )]
         ))
 
+        role2.attach_inline_policy(iam.Policy(self, "lambda-rds-shutdown-policy",
+        statements=[iam.PolicyStatement(
+            actions=["rds:DescribeDBInstances","rds:StopDBInstance"],
+            resources=['*']
+            )]
+        ))
+
+        role3.attach_inline_policy(iam.Policy(self, "lambda-billing-policy",
+        statements=[iam.PolicyStatement(
+            actions=["ce:*"],
+            resources=['*']
+            )]
+        ))
+
+        role3.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSNSFullAccess"))
+        
+        # Create SNS Topic For Billing Lambda
+        sns_topic = sns.Topic(self, "CloudBillingSNSTopic")
+        email = "XXXXXXXXXXXXXXX"
+        sns_topic.add_subscription(subscriptions.EmailSubscription(email))
+
         # Defines an AWS Lambda Resource
         my_lambda = _lambda.Function(
-            self, 'HelloHandler',
+            self, 'Ec2Handler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
-            handler='hello.handler',
-            role = role,
+            handler='ec2.handler',
+            role = role1,
+            timeout=cdk.Duration.minutes(3)
+        )
+        my_lambda2 = _lambda.Function(
+            self, 'rdsHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='rds.handler',
+            role = role2,
             timeout=cdk.Duration.minutes(3)
         )
 
-        # Adding Event Bridge Rule
+        my_lambda3 = _lambda.Function(
+            self, 'billingHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='billing.handler',
+            role = role3,
+            timeout=cdk.Duration.minutes(3)
+        )
+
+        # Grant SNS permissions to Lambda
+        sns_topic.grant_publish(my_lambda3)
+
+        # Add environment variable to Lambda function
+        my_lambda3.add_environment("SNS_TOPIC_ARN", sns_topic.topic_arn)
+
+       
+       # Adding Event Bridge Rule
         rule = events.Rule(self, "Rule",
         schedule=events.Schedule.cron(minute="30", hour="20")
     )
+        rule2 = events.Rule(self, "Rule 2",
+        schedule=events.Schedule.cron(minute="00", hour="09")
+    )
         rule.add_target(targets.LambdaFunction(my_lambda))
+        rule.add_target(targets.LambdaFunction(my_lambda2))
+        rule2.add_target(targets.LambdaFunction(my_lambda3))
 
